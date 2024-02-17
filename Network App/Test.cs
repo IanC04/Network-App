f48b3223b1291cdb0f7ccb9f4524db151bb05cca
Ian <44680390+IanC04@users.noreply.github.com>
2024-02-17 17:46:47 -0600

    Add project files.

---
+++ Network App/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_11_22_oop2
+{
+    public class Calculator
+    {
+        // data portion
+        public int _x, _y;
+
+        // operations
+        public int addInt() => _x + _y;
+        public int subtractInt() => _x - _y;
+        public int multitplyInt() => _x * _y;
+        public int divideInt() => _y == 0 ? 0 : _x / _y;
+    }
+}
+
